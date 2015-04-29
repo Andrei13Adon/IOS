68f8fb56d0a2fac3f2ae32f3a908af299758fa81
Mocioi Andrei Adonis <LaboratoriOS@Cronian-Mac-1.local>
2015-04-29 13:34:51 +0300

    Terminat proiectul mai sunt retusuri

--- MagOnline/MagOnline/AdaugViewController.m
+++ MagOnline/MagOnline/AdaugViewController.m
@@ -8,6 +8,7 @@
 
 #import "AdaugViewController.h"
 #import <Parse/Parse.h>
+#import "SubCategorii.h"
 
 @interface AdaugViewController ()<UITextFieldDelegate>
 
@@ -18,6 +19,8 @@
 @property (strong , nonatomic)UILabel *lNume,*lPrenume,*lAdresa,*lMail,*lTelefon,*lDescriere,*lTitlu,*lPret;
 
 @property (nonatomic, strong)UITextField *tfNume,*tfPrenume,*tfAdresa,*tfMail,*tfTelefon,*tfDescriere,*tfTitlu,*tfPret;
+
+@property (strong,nonatomic) SubCategorii *userState;
 
 @end
 
@@ -39,6 +42,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _userState = [SubCategorii sharedSingleton];
     self.view.backgroundColor = [UIColor blackColor];
     //  CGFloat TextH = self.view.frame.size.height - 20 - 44 - 49  -50 ;
     CGFloat textW = self.view.frame.size.width;
@@ -124,6 +128,14 @@
     _tfPret.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_tfPret];
     _tfPret.delegate =self;
+    
+    if(_userState.Satre == YES){
+        _tfNume.text = _userState.nume;
+        _tfPrenume.text = _userState.prenume;
+        _tfTelefon.text = _userState.numarTelefon;
+        _tfAdresa.text = _userState.adresa;
+        _tfMail.text = _userState.mail;
+    }
     
     UIButton *ConfirmaCumparareaBut = [[UIButton alloc] initWithFrame: CGRectMake(0, self.view.frame.size.height- 20 - 44 - 49 -50, self.view.frame.size.width, 50)];
     ConfirmaCumparareaBut.backgroundColor = [UIColor blueColor];
