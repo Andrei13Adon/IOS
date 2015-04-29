621d13385dd544158437a1286619b68ce13fefd2
Mocioi Andrei Adonis <LaboratoriOS@Cronian-Mac-1.local>
2015-04-29 15:27:00 +0300

    Daugare informatii personale in pagina de log out

--- MagOnline/MagOnline/LogOutViewController.m
+++ MagOnline/MagOnline/LogOutViewController.m
@@ -15,6 +15,8 @@
 @property(strong,nonatomic)SubCategorii *userState;
 @property(strong, nonatomic)UIButton *bLogOut;
 
+@property (strong , nonatomic)UILabel *Descriere,*Username,*titluProdus,*NumeVanzator,*Telefon,*Mail,*Adresa;
+
 @end
 
 @implementation LogOutViewController
@@ -31,7 +33,57 @@
         [_bLogOut setTitle:@"Log Out"forState:UIControlStateNormal];
         [self.view addSubview: _bLogOut];
         [_bLogOut addTarget:self action:@selector(click) forControlEvents: UIControlEventTouchUpInside];
-        //_userState.Satre = NO;
+        
+        //***********//
+        _Username = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width/3, 80, 200 -20, 20)];
+        
+
+        CGFloat plecare = self.view.frame.size.height- 20 - 44 - 49 - 200;
+        _NumeVanzator = [[UILabel alloc] initWithFrame:CGRectMake(20, plecare, self.view.frame.size.width-40, 20)];
+        _Telefon = [[UILabel alloc] initWithFrame:CGRectMake(20, plecare +20, self.view.frame.size.width-40, 20)];
+        _Mail = [[UILabel alloc] initWithFrame:CGRectMake(20, plecare+40, self.view.frame.size.width-40, 20)];
+        _Adresa = [[UILabel alloc] initWithFrame:CGRectMake(20, plecare+60, self.view.frame.size.width-40, 40)];
+
+        _NumeVanzator.textColor = [UIColor whiteColor];
+        _Telefon.textColor = [UIColor whiteColor];
+        _Mail.textColor = [UIColor whiteColor];
+        _Adresa.textColor = [UIColor whiteColor];
+        _Username.textColor = [UIColor whiteColor];
+        
+        [self.view addSubview:_Username];
+        [self.view addSubview:_NumeVanzator];
+        [self.view addSubview:_Telefon];
+        [self.view addSubview:_Mail];
+        [self.view addSubview:_Adresa];
+        
+        
+        
+        NSMutableString *temp;
+        
+        temp =[[NSMutableString alloc]initWithString:@"Username:"];
+        [temp appendString:_userState.userName];
+        _Username.text = temp;
+        
+        temp =[[NSMutableString alloc]initWithString:@"Nume:"];
+        [temp appendString:_userState.nume];
+        [temp appendString:@" "];
+        [temp appendString:_userState.prenume];
+        _NumeVanzator.text = temp;
+        
+        temp =[[NSMutableString alloc]initWithString:@"Telefon:"];
+        [temp appendString:_userState.numarTelefon];
+        _Telefon.text = temp;
+        
+        temp =[[NSMutableString alloc]initWithString:@"MailCreator:"];
+        [temp appendString:_userState.mail];
+        _Mail.text = temp;
+        
+        temp =[[NSMutableString alloc]initWithString:@"Adresa:"];
+        [temp appendString:_userState.adresa];
+        _Adresa.text = temp;
+        _Adresa.numberOfLines = 0;
+        [_Adresa sizeToFit];
+        
     }
 }
 
